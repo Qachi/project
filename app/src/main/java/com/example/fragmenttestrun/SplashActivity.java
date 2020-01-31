a564d9e23e2fe5952a9556ccdef8cf7b5f92cfcd
Qachi <akachukwuonyedikachi@gmail.com>
2020-01-31 01:24:20 +0100

    project 1

---
+++ app/src/main/java/com/example/fragmenttestrun/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.fragmenttestrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logoSlide;
+    TextView qachi;
+    Intent intent;
+    private int SPLASH_TIME_OUT= 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logoSlide = findViewById(R.id.logoSplash);
+        qachi = findViewById(R.id.qachi);
+
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim_slide);
+        logoSlide.startAnimation(animation);
+        qachi.startAnimation(animation);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
